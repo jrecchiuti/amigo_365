b5b9f44413f897ee0d54bdb4b37ce2be7a6aa903
jose <jarecchiuti@gmail.com>
2023-02-22 21:41:44 -0400

    registro de usuario con servidor funcionando y validacion de user

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 const express = require("express")
+const cors= require("cors")
 /*es para traer la libreria de express y asi inicializar el servidor y poder escuchar todas las peticiones de los clientes*/
 const app = express()/*es la variable que va almacenada la aplicacion*/
+app.use(cors())
+app.use(express.json())
+    
+let  users = []
 
+class User
+{
+    constructor(name, email, user, password, age, direction, permissions, id)
+    {
+        this.name=name
+        this.email= email
+        this.user= user
+        this.password= password
+        this.age = age
+        this.direction= direction
+        this.permissions= permissions
+        this.id= id
+    }
+}
+
+
+app.post("/create", (req,res) => {
+    
+    console.log(users)
+
+    let user = new User()
+    let ident = Math.floor(Math.random() * (1000000 - 1 + 1) + 1)
+    let diferente = confirmacionUsuario(req.body.user, users)
+    console.log(diferente)
+    if (diferente ==  0)
+    {
+        users.push(req.body)
+        return res.json({message:'usuario creado con exito'}).status(200).end()/* codigo permite retornar mensaje de respuesta con estatus 200 que indica creacion */
+    
+    }
+    else
+    {
+        return res.json({message:'usuario ya utilizado por favor colocar otro'}).status(200).end()
+    }
+
+}) /*permite recibir informacion del cliente para el servidor */
+/*app.get("/creation", (req,res) => {
+
+
+}) esto permite dar respuesta de forma de lectura al cliente  */
+
+function confirmacionUsuario(usuario, users)
+{
+    let resultado = 0
+    for (let i = 0; i < users.length; i++) {
+       
+        if ( usuario == users[i].user)
+        {
+               resultado = 1
+        } 
+    }
+    return resultado
+}
 app.listen(8080, () => {
     console.log('servidor iniciado')
 })  /* asi se le da el piuerto por el cual se comunicara el servidor con le cliente */
+
--- prueba.js
+++ prueba.js
@@ -1,38 +1,64 @@
-let name = document.getElementById('nombre')
-let password = document.getElementById('clave')
-let edad =  document.getElementById('edad')
-let user = document.getElementById('usuario')
-let email = document.getElementById('email')
-let mayorEdad 
-let ahorro = document.getElementById('ingreso')
-let deuda = document.getElementById('deuda')
-let montoReal
-let nombreCompleto
+let name = document.getElementById("nombre");
+let password = document.getElementById("clave");
+let edad = document.getElementById("edad");
+let user = document.getElementById("usuario");
+let email = document.getElementById("email");
+let permisologia = document.getElementById("permisologia");
+let  direccion = document.getElementById("direccion");
 
-let users= []
 
-class Usuario
-{
-    constructor(name, user, password, email)
+let users = [];
+
+class Usuario {
+    constructor(name, email, user, password , age, direction, permissions, id)
     {
         this.name=name
+        this.email= email
         this.user= user
         this.password= password
-        this.email= email
-
+        this.age = age
+        this.direction= direction
+        this.permissions= permissions
+        this.id= id
     }
 }
 
-function llenar()
-{
-    users.push(new Usuario(name.value, user.value, password.value, email.value)) // manera de llenar un array con el objeto
-    reiniciarFormulario()
-    return
+function llenar() {
+  let usuario = new Usuario(
+    name.value,
+    email.value,
+    user.value,
+    password.value,
+    edad.value,
+    direccion.value,
+    permisologia.value,
+  );
+  users.push(usuario); // manera de llenar un array con el objeto
+  console.log(users);
+  sendDataServer(usuario);
+  reiniciarFormulario();
+  return;
 }
 
-function reiniciarFormulario()
-{
-    let form= document.getElementById('Formulario')
-    form.reset()//es una de las formas para voler los campos vacios de un formulario 
-    return
+function sendDataServer(usuario) {
+  fetch("http://localhost:8080/create", {
+    method: "POST",
+    body: JSON.stringify(usuario),
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  }).then(function (res) {
+    if (res.ok) {
+      res.text().then(function (respuesta) {
+        console.log(respuesta);
+      });
+    }
+  });
+}
+
+function reiniciarFormulario() {
+  let form = document.getElementById("Formulario");
+  form.reset(); //es una de las formas para voler los campos vacios de un formulario
+  return;
 }
